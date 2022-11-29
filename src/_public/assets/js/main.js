8a911a9c593fbd66b0b3401012307440b8d60ec9
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-11-29 13:20:06 +0700

    modal

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -9,8 +9,14 @@ $(document).ready(function(){
 
   
 $('.c-gnav li').hover(function(){
-      $(this).addClass('is-active').siblings().removeClass('is-active');
+      $(this).addClass('is-hover').siblings().removeClass('is-hover');
 })
 $('.c-gnav li').click(function(){
     $(this).addClass('is-active').siblings().removeClass('is-active');
+})
+$('.c-work__modalclose').click(function(){
+    $('.c-work__photomodal').addClass('is-close')
+})
+$('.c-work__image').click(function(){
+    $('.c-work__photomodal').removeClass('is-close')
 })

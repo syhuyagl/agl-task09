8fc45bf076eab4646c96a237433b8901c0b003f6
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-12-02 16:07:06 +0700

    update

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -5,7 +5,18 @@ $(document).ready(function(){
     });
   });
 
-  
+$('.c-header__menu').click(function(){
+    if ($('.c-header__menuiconopen').hasClass('is-show')){
+        $('.c-header__menuiconopen').removeClass('is-show').addClass('is-close')
+        $('.c-header__menuiconclose').removeClass('is-close').addClass('is-show')
+    }
+    else{
+        $('.c-header__menuiconclose').addClass('is-close').removeClass('is-show')
+        $('.c-header__menuiconopen').addClass('is-show').removeClass('is-close')
+    }
+    $('.c-header__submenu').toggleClass('is-open')
+    $('body').toggleClass('is-fixed')
+})
 $('.c-gnav li').hover(function(){
       $(this).addClass('is-hover').siblings().removeClass('is-hover');
 })
@@ -28,3 +39,23 @@ $('.c-intaview__cardimage').click (function(){
     $('.c-intaview__modal').removeClass('is-close')
     $('body').addClass('is-fixed')
 })
+$('.c-work__photomodal').mouseup(function(e) 
+{
+    var container = $(".c-work__photomodalmain");
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        $('.c-work__photomodal').addClass('is-close')
+        $('body').removeClass('is-fixed')
+    }
+});
+$('.c-intaview__modal').mouseup(function(e) 
+{
+    var container = $(".c-intaview__modalmain");
+    // if the target of the click isn't the container nor a descendant of the container
+    if (!container.is(e.target) && container.has(e.target).length === 0) 
+    {
+        $('.c-intaview__modal').addClass('is-close')
+        $('body').removeClass('is-fixed')
+    }
+});

1c7af9eab2ac9c90c4557c02e5f92a1f1aaa1d41
AGL_Hồ Sỹ Huy  (フイ) <ho_sy_huy@allgrow-labo.jp>
2022-12-07 17:32:51 +0700

    fix

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -8,26 +8,30 @@ $(document).ready(function () {
     arrows: false,
   });
   ScrollReveal({
-    reset: true,
-    distance: '25px',
+    reset: false,
+    distance: "25px",
     duration: 1300,
     delay: 200,
-    origin: 'bottom'
+    origin: "bottom",
   });
-  // ScrollReveal().reveal('.c-about__content, .c-work__header, .c-work__introcontent, .c-work__photosheading,.c-work__subheading2, .c-work__content, .c-career__heading, .c-career__subheading, .c-career__banner, .c-career__plan, .c-career__service, .c-intaview,  .c-recruit__tabwrapper, .c-recruit__about', )
+  ScrollReveal().reveal('.c-about__content, .c-work__header, .c-work__introcontent, .c-work__photosheading,.c-work__subheading2, .c-work__content, .c-career__heading, .c-career__subheading, .c-career__banner, .c-career__plan, .c-career__service, .c-intaview,  .c-recruit__tabwrapper, .c-recruit__about', )
 });
 
-
+function toggleMenu() {
+  $(".c-header__menuiconclose").addClass("is-close").removeClass("is-show");
+  $(".c-header__menuiconopen").addClass("is-show").removeClass("is-close");
+  $(".c-header__container").get(0).style.setProperty("--show", "none");
+  $(".c-header__submenu").removeClass("is-open");
+}
 $(".c-header__menu").click(function () {
   if ($(".c-header__menuiconopen").hasClass("is-show")) {
     $(".c-header__menuiconopen").removeClass("is-show").addClass("is-close");
     $(".c-header__menuiconclose").removeClass("is-close").addClass("is-show");
-    $('.c-header__container').get(0).style.setProperty('--show', 'block')
+    $(".c-header__container").get(0).style.setProperty("--show", "block");
   } else {
     $(".c-header__menuiconclose").addClass("is-close").removeClass("is-show");
     $(".c-header__menuiconopen").addClass("is-show").removeClass("is-close");
-    $('.c-header__container').get(0).style.setProperty('--show', 'none')
-
+    $(".c-header__container").get(0).style.setProperty("--show", "none");
   }
   $(".c-header__submenu").toggleClass("is-open");
   $("body").toggleClass("is-fixed");
@@ -68,69 +72,96 @@ $(".c-intaview__modal").mouseup(function (e) {
     $("body").removeClass("is-fixed");
   }
 });
-$('.c-recruit__tab1').click(function(){
-    $('.c-recruit__tabcontent2').addClass('is-hide')
-    $('.c-recruit__tabcontent').removeClass('is-hide')
-    $('.c-recruit__tab1').addClass('is-active')
-    $('.c-recruit__tab2').removeClass('is-active')
-
-
-})
-$(".c-header__firstitem").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".c-about").offset().top
-  }, 1000);
+$(".c-recruit__tab1").click(function () {
+  $(".c-recruit__tabcontent2").addClass("is-hide");
+  $(".c-recruit__tabcontent").removeClass("is-hide");
+  $(".c-recruit__tab1").addClass("is-active");
+  $(".c-recruit__tab2").removeClass("is-active");
 });
-$(".c-about__scroll").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".c-about").offset().top
-  }, 1000);
+$(".c-header__firstitem").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-about").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
 });
-$(".c-header__seconditem").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".c-work").offset().top
-  }, 1000);
+$(".c-about__scroll").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-about").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
 });
-$(".c-header__thirditem").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".c-career").offset().top
-  }, 1000);
+$(".c-header__seconditem").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-work").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
 });
-$(".c-header__fourthitem").click(function() {
-  $('html, body').animate({
-      scrollTop: $(".c-intaview").offset().top
-  }, 1000);
+$(".c-header__thirditem").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-career").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
 });
-$('.c-header__assignment').click(function() {
-  $('html, body').animate({
-      scrollTop: $(".c-recruit").offset().top
-  }, 1000);
+$(".c-header__fourthitem").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-intaview").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
 });
-$(".c-header__fifthitem").click(function() {
-  $('html, body').animate({
-      scrollTop: $('.c-recruit__secondheading').offset().top
-  }, 1000);
+$(".c-header__assignment").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-recruit").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
 });
-$('.c-recruit__tab2').click(function(){
-        $('.c-recruit__tabcontent2').removeClass('is-hide')
-        $('.c-recruit__tabcontent').addClass('is-hide')
-        $('.c-recruit__tab2').addClass('is-active')
-        $('.c-recruit__tab1').removeClass('is-active')
-})
+$(".c-header__fifthitem").click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".c-recruit__secondheading").offset().top,
+    },
+    1000
+  );
+  toggleMenu();
+});
+$(".c-recruit__tab2").click(function () {
+  $(".c-recruit__tabcontent2").removeClass("is-hide");
+  $(".c-recruit__tabcontent").addClass("is-hide");
+  $(".c-recruit__tab2").addClass("is-active");
+  $(".c-recruit__tab1").removeClass("is-active");
+});
 $(window).scroll(function () {
-    /* Check the location of each desired element */
-    let scroll_pos = $(window).scrollTop();
-    $('.p-scroll').each(function(i){
-        if (scroll_pos + 500 >= $(this).offset().top ){
-            $('.c-gnav li').removeClass('is-active')
-            $('.c-gnav li').eq(i).addClass('is-active')
-        }
-    })
+  /* Check the location of each desired element */
+  let scroll_pos = $(window).scrollTop();
+  $(".p-scroll").each(function (i) {
+    if (scroll_pos + 500 >= $(this).offset().top) {
+      $(".c-gnav li").siblings().removeClass("is-active");
+      $(".c-gnav li").eq(i).addClass("is-active");
+    }
   });
-  $(".top").on('click',function() {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 1000);
-   
 });
-
+$(".top").on("click", function () {
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    1000
+  );
+});
